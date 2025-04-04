78afae50f9239aecfe8dfcc13b88a677878f7e2e
Michal <klich.office.git.vps.deploy@gmail.com>
2025-04-04 11:55:43 +0200

    Connected app with MongoDB! wow it works and saving

--- index.js
+++ index.js
@@ -2,48 +2,88 @@ import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import morgan from "morgan";
-
-// reading what user type in form and then sent with button as POST
-import bodyParser from "body-parser";
+import bodyParser from "body-parser"; // reading what user type in form and then sent with button as POST
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 //init 'const' before 'use' section
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 7007;
 
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// log details
-app.use(morgan("tiny"));
-
+app.use(morgan("tiny")); // log details
+app.use(express.static(__dirname + "/public")); // Serve static files
 
 
-// Serve static files
-app.use(express.static(__dirname + "/public"));
+//MongoDB Config
+const uri =
+  "mongodb+srv://michal:MUXoMLT321@cluster0.re1zc8a.mongodb.net/?appName=Cluster0";
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+//hold db instance in variable for futher use
+let db;
+
+//check db connection
+async function connectWithMongo() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+
+    console.log("You successfully connected to MongoDB!");
+    db = client.db("Cluster0"); // or whatever your DB name is
+
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+    process.exit(1); // only exit if it actually failed
+  }
+}
+connectWithMongo().catch(console.dir);
+
+// ROUTES - 
 // Strona glowna startowa - zwraca widok startowy
 app.get("/home", (req, res) => {
-    // console.log(req.rawHeaders); 
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+  // console.log(req.rawHeaders);
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Gdy user klika link na stronie startowej "Dodaj wydatek" - wtedy ten endpoint przejmuje GET i zwraca widok do dodania outcome
 app.get("/add-outcome", (req, res) => {
-    // console.log(req.rawHeaders); 
-    res.sendFile(path.join(__dirname, "public", "addOutcome.html"));
+  // console.log(req.rawHeaders);
+  res.sendFile(path.join(__dirname, "public", "addOutcome.html"));
 });
 
-// Przejmij dane z formularza gdy user klika "Save" i wywoluje POST. 
-app.post("/add-outcome", (req, res) => {
-    console.log("Nowy wydatek, kwota: " + req.body.outcomeAmount);
-    console.log("Nowy wydatek, nazwa: " + req.body["outcomeName"]);
-
+// Przejmij dane z formularza gdy user klika "Save" i wywoluje POST.
+app.post("/add-outcome", async (req, res) => {
+  const outcomeNameDAO = req.body["outcomeName"];
+  try {
+    await db.collection("wydatki").insertOne({
+      outcomeNameDAO,
+      createdAt: new Date(),
+    });
     res.sendFile(path.join(__dirname, "public", "seeTodayOutcomeList.html"));
+  } catch (err) {
+    console.error("Insert failed:", err);
+    res.status(500).send("Something went wrong.");
+  }
 });
 
-// Podstawowy do uruchomienia serwera
+// Uruchomienie serwera
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
- 
+
+// alternatywnie uruchom app tylko gdy DB jest ready
+// Start app only after DB is ready
+// connectWithMongo().then(() => {
+//     app.listen(port, () => {
+//       console.log(`🚀 Server running at http://localhost:${port}`);
+//     });
+//   });
